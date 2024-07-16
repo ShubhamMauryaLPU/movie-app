ecb51ad618ea98a7e33122cec0cb961c83ea0a83
vikas <vikaspatel82916@gmail.com>
2024-07-16 19:34:47 +0530

    second error

--- Componets/Header/Body.jsx
+++ Componets/Header/Body.jsx
@@ -1,9 +1,11 @@
 import "./Body.css";
-export default function Body({theme}){
-    return(
-        <div className="des" style={{color:theme==="dark"&&"white"}}>
-            <h1>Unlimited Movies and Tv Shows </h1>
-            <p>Download Now OR Watch Online</p>
-        </div>
-    );
+export default function Body({ theme }) {
+  return (
+    <>
+      <div className="des" style={{ color: theme === "dark" && "white" }}>
+        <h1>Unlimited Movies and Tv Shows </h1>
+        <p>Download Now OR Watch Online</p>
+      </div>
+    </>
+  );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import Navbar from "../Componets/Header/Navbar";
+import Footer from "../Componets/Header/Footer";
 import FirstPage from "../Componets/FirstPage";
 function App() {
-  const current_theme=localStorage.getItem("current_theme");
-  const [theme,setTheme]=useState(current_theme?current_theme:"light");
-  useEffect(()=>{
-    localStorage.setItem("current_theme",theme);
-  },[theme]);
+  const current_theme = localStorage.getItem("current_theme");
+  const [theme, setTheme] = useState(current_theme ? current_theme : "light");
+  useEffect(() => {
+    localStorage.setItem("current_theme", theme);
+  }, [theme]);
   return (
-    <div className={`container ${theme}`}>
-      <FirstPage theme={theme} setTheme={setTheme}/>
-    </div>
-  )
+    <>
+      <div className={`container ${theme}`}>
+        <FirstPage theme={theme} setTheme={setTheme} />
+      </div>
+      <div>
+        <Footer theme={theme} />{" "}
+      </div>
+    </>
+  );
 }
 
 export default App;
